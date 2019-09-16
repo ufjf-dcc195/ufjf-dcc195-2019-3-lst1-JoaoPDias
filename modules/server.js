5aed254857fa5c3e7b7e0176a57ad06f43111b58
JoaoPDias <jpdias1997@hotmail.com>
2019-09-16 01:13:02 -0300

    Adição da Porta Runtime

--- modules/server.js
+++ modules/server.js
@@ -1,11 +1,11 @@
 let http = require("http");
 let url = require("url");
-const port = process.env.PORT || 5000
 function start(route,routes) {
     let server = http.createServer(function (req,res) {
         route(url.parse(req.url).pathname,routes,req,res);
     });
-    server.listen(port);
-    console.log(`Listening on ${ port }`);
+    server.listen(process.env.PORT || 3000 ,function(){
+        console.log("up and running on port "+process.env.PORT);
+    })
 }
 exports.start = start;
